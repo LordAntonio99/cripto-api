77b5c58c8cf123972bdbebd491d5362c6183dc35
Antonio <tonicintora@gmail.com>
2022-08-02 21:54:59 +0200

    added info to coins

--- routes/info.js
+++ routes/info.js
@@ -35,32 +35,102 @@ router.get("/:id", async (req, res) => {
     )
       .text()
       .replaceAll(",", "");
+    const low7d = $(
+      "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.jKrmxw.container > div > div.sc-16r8icm-0.sc-19zk94m-1.gRSJaB > div:nth-child(3) > div > div.sc-16r8icm-0.hgKnTV > section > div > div.sc-16r8icm-0.kjciSH.hide > div:nth-child(2) > table > tbody > tr:nth-child(1) > td > div:nth-child(1)"
+    )
+      .text()
+      .replace("/", "")
+      .replace(" ", "")
+      .replaceAll(",", "");
+    const high7d = $(
+      "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.jKrmxw.container > div > div.sc-16r8icm-0.sc-19zk94m-1.gRSJaB > div:nth-child(3) > div > div.sc-16r8icm-0.hgKnTV > section > div > div.sc-16r8icm-0.kjciSH.hide > div:nth-child(2) > table > tbody > tr:nth-child(1) > td > div:nth-child(2)"
+    )
+      .text()
+      .replaceAll(",", "");
+    const low30d = $(
+      "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.jKrmxw.container > div > div.sc-16r8icm-0.sc-19zk94m-1.gRSJaB > div:nth-child(3) > div > div.sc-16r8icm-0.hgKnTV > section > div > div.sc-16r8icm-0.kjciSH.hide > div:nth-child(2) > table > tbody > tr:nth-child(2) > td > div:nth-child(1)"
+    )
+      .text()
+      .replace("/", "")
+      .replace(" ", "")
+      .replaceAll(",", "");
+    const high30d = $(
+      "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.jKrmxw.container > div > div.sc-16r8icm-0.sc-19zk94m-1.gRSJaB > div:nth-child(3) > div > div.sc-16r8icm-0.hgKnTV > section > div > div.sc-16r8icm-0.kjciSH.hide > div:nth-child(2) > table > tbody > tr:nth-child(2) > td > div:nth-child(2)"
+    )
+      .text()
+      .replaceAll(",", "");
+    const low90d = $(
+      "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.jKrmxw.container > div > div.sc-16r8icm-0.sc-19zk94m-1.gRSJaB > div:nth-child(3) > div > div.sc-16r8icm-0.hgKnTV > section > div > div.sc-16r8icm-0.kjciSH.hide > div:nth-child(2) > table > tbody > tr:nth-child(3) > td > div:nth-child(1)"
+    )
+      .text()
+      .replace("/", "")
+      .replace(" ", "")
+      .replaceAll(",", "");
+    const high90d = $(
+      "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.jKrmxw.container > div > div.sc-16r8icm-0.sc-19zk94m-1.gRSJaB > div:nth-child(3) > div > div.sc-16r8icm-0.hgKnTV > section > div > div.sc-16r8icm-0.kjciSH.hide > div:nth-child(2) > table > tbody > tr:nth-child(3) > td > div:nth-child(2)"
+    )
+      .text()
+      .replaceAll(",", "");
+    const low1yr = $(
+      "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.jKrmxw.container > div > div.sc-16r8icm-0.sc-19zk94m-1.gRSJaB > div:nth-child(3) > div > div.sc-16r8icm-0.hgKnTV > section > div > div.sc-16r8icm-0.kjciSH.hide > div:nth-child(2) > table > tbody > tr:nth-child(4) > td > div:nth-child(1)"
+    )
+      .text()
+      .replace("/", "")
+      .replace(" ", "")
+      .replaceAll(",", "");
+    const high1yr = $(
+      "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.jKrmxw.container > div > div.sc-16r8icm-0.sc-19zk94m-1.gRSJaB > div:nth-child(3) > div > div.sc-16r8icm-0.hgKnTV > section > div > div.sc-16r8icm-0.kjciSH.hide > div:nth-child(2) > table > tbody > tr:nth-child(4) > td > div:nth-child(2)"
+    )
+      .text()
+      .replaceAll(",", "");
+    const allTimeHigh = $(
+      "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.jKrmxw.container > div > div.sc-16r8icm-0.sc-19zk94m-1.gRSJaB > div:nth-child(3) > div > div.sc-16r8icm-0.hgKnTV > section > div > div.sc-16r8icm-0.kjciSH.hide > div:nth-child(2) > table > tbody > tr:nth-child(5) > td > span"
+    )
+      .text()
+      .replaceAll(",", "");
+    const allTimeLow = $(
+      "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.jKrmxw.container > div > div.sc-16r8icm-0.sc-19zk94m-1.gRSJaB > div:nth-child(3) > div > div.sc-16r8icm-0.hgKnTV > section > div > div.sc-16r8icm-0.kjciSH.hide > div:nth-child(2) > table > tbody > tr:nth-child(6) > td > span"
+    )
+      .text()
+      .replaceAll(",", "");
     const marketPercent = $(
       "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.jKrmxw.container > div > div.sc-16r8icm-0.sc-19zk94m-1.gRSJaB > div:nth-child(3) > div > div.sc-16r8icm-0.hgKnTV > section > div > div:nth-child(2) > table > tbody > tr:nth-child(6) > td > span"
+    ).text();
+    const coinROI = $(
+      "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.jKrmxw.container > div > div.sc-16r8icm-0.sc-19zk94m-1.gRSJaB > div:nth-child(3) > div > div.sc-16r8icm-0.hgKnTV > section > div > div.sc-16r8icm-0.kjciSH.hide > div:nth-child(2) > table > tbody > tr:nth-child(7) > td > div "
     ).text();
     const marketPosition = $(
       "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.jKrmxw.container > div > div.sc-16r8icm-0.sc-19zk94m-1.gRSJaB > div:nth-child(3) > div > div.sc-16r8icm-0.hgKnTV > section > div > div:nth-child(2) > table > tbody > tr:nth-child(7) > td"
     )
       .text()
       .replace("#", "");
-    // let markets = [] 
-    // $(
-    //   "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.jKrmxw.container > div > div:nth-child(2) > div.sc-10p9viq-3.jfTMrS > div > table > tbody"
-    // ).each((i, e) => {
-    //     $(this).find("td").each((i,e) => {
-    //         markets.push($(this).text().trim())
-    //     })
-    // })
-    // console.log(markets);
+    const totalCoinSupply = $(
+      "#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div.sc-16r8icm-0.jKrmxw.container > div > div.sc-16r8icm-0.sc-19zk94m-1.gRSJaB > div:nth-child(3) > div > div.sc-16r8icm-0.hgKnTV > section > div > div.sc-16r8icm-0.kjciSH.hide > div:nth-child(3) > table > tbody > tr:nth-child(1) > td"
+    )
+      .text()
+      .split(" ")[0]
+      .replaceAll(",", "");
     const data = {
       price,
       priceChange24h,
       tradingVolume24h,
       low24h,
       high24h,
+      low7d,
+      high7d,
+      low30d,
+      high30d,
+      low90d,
+      high90d,
+      low1yr,
+      high1yr,
+      allTimeHigh,
+      allTimeLow,
+      coinROI,
       marketPercent,
       marketPosition,
-    //   markets,
+      totalCoinSupply,
+      //   markets,
     };
     res.status(200).json(data);
   } catch (error) {
